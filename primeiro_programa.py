445216fa6295127faf8c68bc09edc92a659a21fb
Paulo <paulodemetrio1982@gmail.com>
2023-07-12 04:32:37 -0300

    Estudando a trilha python na DIO, Potência tech powered by Ifoof.

---
+++ primeiro_programa.py
@@ -0,0 +1,29 @@
+print ('Hello World!')
+
+# Tipo de dados
+
+int()
+float()
+bool()
+str()
+
+# Váriaveis
+
+nome = 'Paulo'
+idade = 40
+saldo = 1050.36
+
+print (f'nome:{nome}, idade:{idade}, saldo:{saldo}')
+
+# Conversão
+
+print (int(10.545))
+print (float(10.10))
+print (int('23'))
+print (type(str(10.23)))
+
+#Função de entrada e saída
+
+nomes = input ('Informe seu nome: ')
+print(f'olá {nomes}')
+
